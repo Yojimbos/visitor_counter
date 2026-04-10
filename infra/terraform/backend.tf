bcfc1c17b246201e2f46f9bf76dae991e3fbbb19
Yojimbos <karahanah87@gmail.com>
2026-04-10 19:08:06 +0300

    fix pipepline

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "visitor-counter-rg"
+    storage_account_name = "visitorcounterstate"
+    container_name       = "tfstate"
+    key                  = "visitor-counter.terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
