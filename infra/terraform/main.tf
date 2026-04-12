cabe4e8e49b8fbf4fb336587625a7af0e5b93e3f
Yojimbos <karahanah87@gmail.com>
2026-04-12 10:26:43 +0300

    remove rbac

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -205,16 +205,3 @@ resource "azurerm_key_vault_secret" "acr_login_server" {
     ignore_changes = all
   }
 }
-
-# RBAC for AKS to access ACR
-resource "azurerm_role_assignment" "aks_acr" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-  
-  lifecycle {
-    ignore_changes = all
-    create_before_destroy = true
-  }
-}
