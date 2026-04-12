c8d560a1fc0acfc28f5f0620a364c3c0ebe37ade
Yojimbos <karahanah87@gmail.com>
2026-04-12 19:31:35 +0300

    sitecore_aks

--- infra/terraform/ingress.tf
+++ infra/terraform/ingress.tf
@@ -1,8 +1,15 @@
+resource "kubernetes_namespace_v1" "ingress_nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+  depends_on = [azurerm_kubernetes_cluster.aks]
+}
+
 resource "helm_release" "ingress_nginx" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = "default"
+  namespace  = kubernetes_namespace_v1.ingress_nginx.metadata[0].name
 
   wait    = true
   timeout = 600
@@ -15,6 +22,5 @@ resource "helm_release" "ingress_nginx" {
     { name = "controller.admissionWebhooks.patch.nodeSelector.kubernetes\\.io/os", value = "linux" },
     { name = "controller.service.externalTrafficPolicy", value = "Local" },
   ]
-
-  depends_on = [azurerm_kubernetes_cluster.sitecore_aks]
+   depends_on = [kubernetes_namespace_v1.ingress_nginx]
 }
