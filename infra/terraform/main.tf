c2d71d96e19415258f2c96f86268ece58b0cb763
Yojimbos <karahanah87@gmail.com>
2026-04-13 09:18:18 +0300

    remove ingress

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 data "azurerm_client_config" "current" {}
 
 locals {
